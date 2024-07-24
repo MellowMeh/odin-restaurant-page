b08ac2c53e5b92e5e2a8aaae93f520061856e59d
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-07-24 13:02:04 -0400

    Fix webpack relative path

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/script/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: './src/index.html'
+        template: './src/page/index.html'
     })
   ]
 };
