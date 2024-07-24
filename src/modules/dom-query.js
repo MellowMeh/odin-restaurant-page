fdca906ad9d67137f2d657295349c8df36a3742c
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-07-24 12:29:28 -0400

    Add dom-query module
    Query selectors have been written for each HTML
    element, before any DOM manipulation.

---
+++ src/modules/dom-query.js
@@ -0,0 +1,15 @@
+let header = document.querySelector('header');
+let nav = document.querySelector('nav');
+let homeButton = document.querySelector('#home');
+let menuButton = document.querySelector('#menu');
+let aboutButton = document.querySelector('#about');
+let contentContainer = document.querySelector('#content');
+
+export {
+    header,
+    nav,
+    homeButton,
+    menuButton,
+    aboutButton,
+    contentContainer
+};
