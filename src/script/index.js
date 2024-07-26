e38f6cb9db463723ddf551409d8e125c6351afc9
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-07-26 16:24:23 -0400

    Make initial load a  function
    For readability sake, the functions needed to build the
    homepage have been organized into a single self-executing
    function.

--- src/script/index.js
+++ src/script/index.js
@@ -1,7 +1,9 @@
 import '../style/index.css';
 import { generateBackground, generateLogo, generateNav } from '../modules/homepage-initial-load';
 
+const generateHomePage = (function () {
+    generateLogo();
+    generateNav();
+    generateBackground();
+})();
 
-generateLogo();
-generateNav();
-generateBackground();
